2af7e063e95c5576cd856eb5ab85fb2cd4afbbf2
Harshit Tyagi <tyagiharshit416@gmail.com>
2023-03-12 15:48:19 +0530

    index and profile changed

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Profile = () => {
+const Profile = (props) => {
   return (
-    <div class="p-3">
+    <section class="p-3">
       <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
         <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-xl md:rounded-r-none rounded-xl mx-auto " src={process.env.PUBLIC_URL + '/images/sarah-dayan.jpg'} alt="" width="384" height="512" />
-        <div class="pt-6 md:p-8 text-center md:text-left space-y-4 grow">
+        <div class="pt-6 md:p-8 text-center md:text-left space-y-3 grow">
           <figcaption class="font-medium grid md:grid-cols-3 md:gap-6">
             <div class="text-sky-500 dark:text-sky-400">
               Sarah Dayan
@@ -16,7 +16,7 @@ const Profile = () => {
           </figcaption>
         </div>
       </figure>
-    </div>
+    </section>
   )
 }
 
