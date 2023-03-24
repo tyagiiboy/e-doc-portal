78474a6491c2b41eda209cc459c0e1432336ff06
Harshit Tyagi <tyagiharshit416@gmail.com>
2023-03-24 10:12:59 +0530

    demo was extra

--- demo/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Stack;
-
-public class Main {
-  public static void main(String[] args) {
-    char[] ar = {'0', '0'};
-    char c = '0';
-    if (ar[0] == c) System.out.println("true");
-  }
-}
